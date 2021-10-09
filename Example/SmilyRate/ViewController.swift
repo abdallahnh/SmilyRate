8b1bf71ebb9199942ad283fecd08ea8cb10f809f
Abdallah Nehme <anehme@totersapp.com>
2021-10-09 20:11:35 +0300

    Populate default data

--- Example/SmilyRate/ViewController.swift
+++ Example/SmilyRate/ViewController.swift
@@ -15,14 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let sm1 = SmilModel(image: UIImage(imageLiteralResourceName: "1"), title: "Very Satisfied")
-        let sm2 = SmilModel(image: UIImage(imageLiteralResourceName: "2"), title: "Satisfied")
-        let sm3 = SmilModel(image: UIImage(imageLiteralResourceName: "3"), title: "Neutral")
-        let sm4 = SmilModel(image: UIImage(imageLiteralResourceName: "4"), title: "Dissatisfied")
-        let sm5 = SmilModel(image: UIImage(imageLiteralResourceName: "5"), title: "Very Dissatisfied")
-
-        self.smilyRateView.setSmiling(smiling: [sm1, sm2, sm3, sm4, sm5])
     }
 
     override func didReceiveMemoryWarning() {
--- SmilyRate/Classes/View/SmilyRateView.swift
+++ SmilyRate/Classes/View/SmilyRateView.swift
@@ -15,16 +15,37 @@ public class SmilyRateView: UIView {
 
     public override func awakeFromNib() {
         super.awakeFromNib()
+        self.setupView()
+        self.populateDefaultData()
     }
     // MARK: - Public Functions
     public func setSmiling(smiling: [SmilModel]){
         for smil in smiling {
-            let stackView = ANStackView(style: .v(distribution: .equalSpacing, spacing: 2)).view
+            let stackView = ANStackView(style: .v(distribution: .fillEqually, spacing: 2)).view
             let imageView = UIImageView(image: smil.image)
-            let label = UILabel()
+            let containerView = UIView()
 
-            stackView.addArrangedSubview(imageView)
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 13)
+            label.numberOfLines = 2
+            label.lineBreakMode = .byWordWrapping
+            label.textAlignment = .center
+            label.text = smil.title
+
+            containerView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+
+            stackView.addArrangedSubview(containerView)
             stackView.addArrangedSubview(label)
+
+            containerView.addSubview(imageView)
+
+            NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 35).isActive = true
+            NSLayoutConstraint(item: containerView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 35).isActive = true
+
+            NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: containerView, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+            NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: containerView, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+
             self.stackView.addArrangedSubview(stackView)
         }
     }
@@ -48,5 +69,14 @@ public class SmilyRateView: UIView {
         ]
 
         _ = constraints.map({$0.isActive = true})
+    }
+    private func populateDefaultData(){
+        let sm1 = SmilModel(image: UIImage(imageLiteralResourceName: "1"), title: "Very Satisfied")
+        let sm2 = SmilModel(image: UIImage(imageLiteralResourceName: "2"), title: "Satisfied")
+        let sm3 = SmilModel(image: UIImage(imageLiteralResourceName: "3"), title: "Neutral")
+        let sm4 = SmilModel(image: UIImage(imageLiteralResourceName: "4"), title: "Dissatisfied")
+        let sm5 = SmilModel(image: UIImage(imageLiteralResourceName: "5"), title: "Very Dissatisfied")
+
+        self.setSmiling(smiling: [sm1, sm2, sm3, sm4, sm5])
     }
 }
