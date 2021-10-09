058fae6b0392cdfa470ea78bd9bf6353dfa68583
Abdallah Nehme <anehme@totersapp.com>
2021-10-09 19:12:30 +0300

    Added the images and update the example

--- Example/SmilyRate/ViewController.swift
+++ Example/SmilyRate/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 import SmilyRate
 
 class ViewController: UIViewController {
-
+   
     @IBOutlet weak var smilyRateView: SmilyRateView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.smilyRateView = SmilyRateView()
-        //let smilModel = SmilModel(
-        //self.smilyRateView.setSmiling(smiling: [SmilModel])
+
+        let sm1 = SmilModel(image: UIImage(imageLiteralResourceName: "1"), title: "Very Satisfied")
+        let sm2 = SmilModel(image: UIImage(imageLiteralResourceName: "2"), title: "Satisfied")
+        let sm3 = SmilModel(image: UIImage(imageLiteralResourceName: "3"), title: "Neutral")
+        let sm4 = SmilModel(image: UIImage(imageLiteralResourceName: "4"), title: "Dissatisfied")
+        let sm5 = SmilModel(image: UIImage(imageLiteralResourceName: "5"), title: "Very Dissatisfied")
+
+        self.smilyRateView.setSmiling(smiling: [sm1, sm2, sm3, sm4, sm5])
     }
 
     override func didReceiveMemoryWarning() {
