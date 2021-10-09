71e5b553d68069c156de6b5950596391b10761ea
Abdallah Nehme <anehme@totersapp.com>
2021-10-09 19:47:37 +0300

    added files

---
+++ SmilyRate/Classes/Model/SmilModel.swift
@@ -0,0 +1,17 @@
+//
+//  SmilModel.swift
+//  SmilyRate
+//
+//  Created by Abdallah Nehme on 09/10/2021.
+//
+
+import Foundation
+public final class SmilModel{
+    let image: UIImage!
+    let title: String!
+
+    public init(image: UIImage, title: String){
+        self.image = image
+        self.title = title
+    }
+}
